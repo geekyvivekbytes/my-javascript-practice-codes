202274452ad354b4a25d47e708394e860ebf477f
geekyvivekbytes <vivekbhardwaj2200@gmail.com>
2025-09-19 14:12:21 +0530

    Create practice_of_switch_function.js

---
+++ practice_of_switch_function.js
@@ -0,0 +1,14 @@
+const roll_no = 34;
+switch (roll_no) {
+    case 32:
+        console.log('name of the student is vivek.'); 
+        break;
+    case 33:
+        console.log('name of the student is rajesh.');
+        break;
+    case 34:
+        console.log('name of the student is rahul.');
+        break;
+    default:
+        console.log('roll_no not found');     
+}
