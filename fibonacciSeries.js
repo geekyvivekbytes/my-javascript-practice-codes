30dcd5138c7446fc6db54f726815eabd03e22772
geekyvivekbytes <vivekbhardwaj2200@gmail.com>
2025-09-19 14:27:34 +0530

    I change current_term to currentTerm

--- fibonacciSeries.js
+++ fibonacciSeries.js
@@ -1,18 +1,18 @@
-let current_term  = 1;
+let currentTerm  = 1;
 let firstTerm  = 0;
 const totalTermRequired = 8;
 
-console.log(current_term, ' st term of fibonacci series is' , firstTerm);
+console.log(currentTerm, ' st term of fibonacci series is' , firstTerm);
 
-current_term = 2;
+currentTerm = 2;
 let secondTerm = 1;
-console.log(current_term , ' nd term of fibonacci series is ', secondTerm);
+console.log(currentTerm , ' nd term of fibonacci series is ', secondTerm);
 
-current_term = 3;
+currentTerm = 3;
 let thirdTerm = secondTerm + firstTerm ;
-console.log(current_term , ' rd term of fibonacci series is ', thirdTerm);
+console.log(currentTerm , ' rd term of fibonacci series is ', thirdTerm);
 
-for (let loopTerm = current_term + 1 ;loopTerm < totalTermRequired; loopTerm = loopTerm + 1){
+for (let loopTerm = currentTerm + 1 ;loopTerm < totalTermRequired; loopTerm = loopTerm + 1){
     
     firstTerm = secondTerm;
     secondTerm = thirdTerm;
@@ -20,3 +20,4 @@ for (let loopTerm = current_term + 1 ;loopTerm < totalTermRequired; loopTerm = l
     console.log(loopTerm , ' th term of fibonacci series is ', thirdTerm);
   
 }
+
