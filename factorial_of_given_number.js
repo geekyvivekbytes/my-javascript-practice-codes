d0a0ec05c7d93bf217c39340dc19ae6ac6301303
geekyvivekbytes <vivekbhardwaj2200@gmail.com>
2025-09-19 14:40:55 +0530

    Create factorial_of_given_number.js

---
+++ factorial_of_given_number.js
@@ -0,0 +1,6 @@
+const givenNumber = 6;
+let result = 1
+for(let count = 1 ; count <= givenNumber ; count = count + 1){
+    result = result * count
+}
+console.log('factorial of ', givenNumber ,'is' , result);
